25a8759c5f0659b10ae0135768be7050c84cfba1
Artem Istomin <istomin010593@gmail.com>
2022-08-18 22:09:58 +0000

    Added repositoru, service

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,14 @@ import (
 
 	todo "github.com/istomin10593/reminder_todo"
 	"github.com/istomin10593/reminder_todo/pck/handler"
+	"github.com/istomin10593/reminder_todo/pck/repository"
+	"github.com/istomin10593/reminder_todo/pck/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.Newepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
