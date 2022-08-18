295258603f978af919cd093819a721190ee49ab4
istomin10593 <istomin.artem.10593@gmail.com>
2022-08-18 23:46:36 +0000

    Added configs

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,14 @@ import (
 	"github.com/istomin10593/reminder_todo/pck/handler"
 	"github.com/istomin10593/reminder_todo/pck/repository"
 	"github.com/istomin10593/reminder_todo/pck/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
+
 	repos := repository.Newepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
@@ -18,4 +23,10 @@ func main() {
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
