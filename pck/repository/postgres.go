368e73bbdb7ff8a319bae71124ef700022165f08
istomin10593 <istomin.artem.10593@gmail.com>
2022-08-23 19:47:49 +0000

    Added repository postgres

---
+++ pck/repository/postgres.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Config struct {
+	Host     string
+	Port     string
+	Username string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
+	db, err1 := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
+	if err1 != nil {
+		return nil, err1
+	}
+
+	err2 := db.Ping()
+	if err2 != nil {
+		return nil, err2
+	}
+
+	return db, nil
+}
