d1fc392cfd98098caba6332122ea5a97d51f8ffd
istomin10593 <istomin.artem.10593@gmail.com>
2022-09-03 13:26:07 +0000

    Fixed update list

--- pck/repository/todo_list_postgres.go
+++ pck/repository/todo_list_postgres.go
@@ -71,18 +71,18 @@ func (r *TodoListPostgres) Delete(userId, listId int) error {
 }
 
 func (r *TodoListPostgres) Update(userId, listId int, input todo.UpdateListInput) error {
-	setValue := make([]string, 0)
+	setValues := make([]string, 0)
 	args := make([]interface{}, 0)
 	argId := 1
 
 	if input.Title != nil {
-		setValue = append(setValue, fmt.Sprintf("title=$%d", argId))
+		setValues = append(setValues, fmt.Sprintf("title=$%d", argId))
 		args = append(args, *input.Title)
 		argId++
 	}
 
 	if input.Description != nil {
-		setValue = append(setValue, fmt.Sprintf("description=$%d", argId))
+		setValues = append(setValues, fmt.Sprintf("description=$%d", argId))
 		args = append(args, *input.Description)
 		argId++
 	}
