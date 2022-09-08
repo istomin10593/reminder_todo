bf2e535215d8f9599e476d99d89629228f7e9552
istomin10593 <istomin.artem.10593@gmail.com>
2022-09-08 00:57:01 +0000

    Added TodoItem create

---
+++ pck/service/todo_item.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	todo "github.com/istomin10593/reminder_todo"
+	"github.com/istomin10593/reminder_todo/pck/repository"
+)
+
+type TodoItemService struct {
+	repo     repository.TodoItem
+	listRepo repository.TodoList
+}
+
+func NewTodoItemService(repo repository.TodoItem, listRepo repository.TodoList) *TodoItemService {
+	return &TodoItemService{repo: repo, listRepo: listRepo}
+}
+
+func (s *TodoItemService) Create(userId, listId int, item todo.TodoItem) (int, error) {
+	_, err := s.listRepo.GetById(userId, listId)
+	if err != nil {
+		// list does not exists or does not belongs to user
+		return 0, err
+	}
+
+	return s.repo.Create(listId, item)
+}
